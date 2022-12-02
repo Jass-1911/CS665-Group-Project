acedbbbe698ac7e20d05fdc27735850e7e5a2341
Jacob Schonhoff <2019jcschonhoff@gmail.com>
2022-12-01 23:01:39 -0600

    Tab 2 & Tab 5 Added
    Tab 2 is semi-working. Listing the products from a specific supplierID works; however, still trying to figure out how to take input for supplierName to list supplier info.
    
    Tab 5 Created but Not Started for Predefined SQL Function Buttons

--- appCRUD.py
+++ appCRUD.py
@@ -33,10 +33,13 @@ tab1 = tk.Frame(nb)
 tab2 = tk.Frame(nb)
 tab3 = tk.Frame(nb)
 tab4 = tk.Frame(nb)
+tab5 = tk.Frame(nb)
 
 # ---- TAB CONTENT ----
 # Each tab is divided into two equal parts: left and right. Left can be used for text entry, buttons, filters, etc.
 # Right can be used to output a table or a result message box
+
+
 
 # ---- Create New Record (Tab 1) ----
 tab1left = tk.Frame(tab1, width=350)
@@ -110,7 +113,7 @@ for product in dbCursor:
         e.insert(tk.END, product[j])
     i = i + 1
 
-# generate customers table
+# generate Product table
 def showProductTable(tableFrameTab1):
     dbCursor.execute("SELECT * FROM Product")
     i = 0
@@ -144,6 +147,17 @@ tab1Btn = tk.Button(tab1left, command=lambda: addProduct(), text="Add Clothing",
                     font=('Arial', 14, 'bold')).pack(side='top', anchor='nw', padx=10, pady=10)
 
 
+
+
+
+
+
+
+
+
+
+
+
 # ---- Read (Tab 2) ----
 tab2left = tk.Frame(tab2, width=350)
 tab2right = tk.Frame(tab2, width=350)
@@ -155,32 +169,102 @@ allTab2Entries = tk.Frame(tab2left)
 allTab2Entries.pack(side='top', pady=10)
 
 tab2Frame1=tk.Frame(allTab2Entries)
-supplierName = tk.Label(tab2Frame1, text="Supplier Name", font=('Arial', 12)).pack(side='left')
-supplierNameEntry = tk.Entry(tab2Frame1, width=20)
-supplierNameEntry.pack(side='left')
+supplierID = tk.Label(tab2Frame1, text="Supplier ID", font=('Arial', 12)).pack(side='left')
+supplierIDEntry = tk.Entry(tab2Frame1, width=20)
+supplierIDEntry.pack(side='left')
 tab2Frame1.pack(side='top', pady=10)
 
 tab2Frame2=tk.Frame(allTab2Entries)
-address=tk.Label(tab2Frame2, text="Address", font=('Arial', 12)).pack(side='left')
-addressEntry=tk.Entry(tab2Frame2, width=20)
-addressEntry.pack(side='left')
+supplierName = tk.Label(tab2Frame2, text="Supplier Name", font=('Arial', 12)).pack(side='left')
+supplierNameEntry = tk.Entry(tab2Frame2, width=20)
+supplierNameEntry.pack(side='left')
 tab2Frame2.pack(side='top', pady=10)
 
-tab2Frame3=tk.Frame(allTab2Entries)
-numOfProd=tk.Label(tab2Frame3, text="Number of Products", font=('Arial', 12)).pack(side='left')
-numOfProdEntry=tk.Entry(tab2Frame3,width=20)
-numOfProdEntry.pack(side='left')
-tab2Frame3.pack(side='top', pady=10)
 
-#"SELECT * FROM <table name> [WHERE <condition>]"
-def readTables():
-    upQuery = "SELECT '%s', '%s', '%s' FROM Suppliers" % (supplierNameEntry.get(), addressEntry.get(), numOfProdEntry.get())
+
+tab2title = tk.Label(tab2right, text="Results", font=('Arial', 16, 'bold')).pack(side='top', anchor='nw', padx=1, pady=1)
+tab2headers = tk.Frame(tab2right, padx=30)
+col21 = tk.Entry(tab2headers,bg='#89CFF0',width=4)
+col21.grid(row=0, column=0)
+col21.insert(tk.END, "Type")
+col22 = tk.Entry(tab2headers,bg='#89CFF0',width=10)
+col22.grid(row=0, column=1)
+col22.insert(tk.END, "SupplierID")
+col23 = tk.Entry(tab2headers,bg='#89CFF0', width=4)
+col23.grid(row=0, column=2)
+col23.insert(tk.END, "Size")
+col24 = tk.Entry(tab2headers,bg='#89CFF0', width=7)
+col24.grid(row=0, column=3)
+col24.insert(tk.END, "Material")
+col25 = tk.Entry(tab2headers, bg='#89CFF0', width=5)
+col25.grid(row=0, column=4)
+col25.insert(tk.END, "Price")
+tab2headers.pack(side='top')
+
+tab2headers2 = tk.Frame(tab2right, padx=30)
+col212 = tk.Entry(tab2headers2,bg='#89CFF0',width=15)
+col212.grid(row=0, column=0)
+col212.insert(tk.END, "Supplier Name")
+col222 = tk.Entry(tab2headers2,bg='#89CFF0',width=10)
+col222.grid(row=0, column=1)
+col222.insert(tk.END, "Address")
+col232 = tk.Entry(tab2headers2,bg='#89CFF0', width=15)
+col232.grid(row=0, column=2)
+col232.insert(tk.END, "# of Products")
+tableFrameTab2 = tk.Frame(tab2right)
+tableFrameTab2.pack(side='top')
+tab2headers2.pack(side='right')
+
+#Find Products with Supplier ID Entry
+def selectProducts():
+    if not supplierIDEntry.get():
+        messagebox.showinfo("Request not complete", "Please enter Supplier ID")
+        return
+    upQuery = "SELECT * FROM Product WHERE supplierID=%s" % (supplierIDEntry.get())
     print(upQuery)
     dbCursor.execute(upQuery)
+    i = 0
+    for product in dbCursor:
+        for j in range(len(product)):
+            e = tk.Entry(tableFrameTab2, width=(len(product)))
+            e.grid(row=i, column=j)
+            e.insert(tk.END, product[j])
+        i = i + 1
+    tableFrameTab2.update()
+    
     db_connection.commit() #NEEDED TO MAKE ACTUAL CHANGES TO DATABASE
 
+tab2Btn1 = tk.Button(tab2Frame1, command=lambda: selectProducts(), text="List Products From Supplier", bg="#274D8B", fg='white', font=('Arial', 8, 'bold')).pack(side='left', anchor='nw', padx=5, pady=5)
 
-tab2Btn = tk.Button(tab2left, command=lambda: readTables(), text="Search", bg="#274D8B", fg='white', font=('Arial', 14, 'bold')).pack(side='top', anchor='nw', padx=10, pady=10)
+def displaySupplierInfo():
+    if not supplierNameEntry.get():
+        messagebox.showinfo("Request not complete", "Please enter Supplier Name")
+        return
+    upQuery = "SELECT * FROM Suppliers WHERE supplierName=%s" % (supplierNameEntry.get())
+    print(upQuery)
+    dbCursor.execute(upQuery)
+    i = 0
+    for supplier in dbCursor:
+        for j in range(len(supplier)):
+            e = tk.Entry(tableFrameTab2, width=(len(supplier)))
+            e.grid(row=i, column=j)
+            e.insert(tk.END, supplier[j])
+        i = i + 1
+    tableFrameTab2.update()
+    
+    db_connection.commit() #NEEDED TO MAKE ACTUAL CHANGES TO DATABASE
+
+tab2Btn2 = tk.Button(tab2Frame2, command=lambda: displaySupplierInfo(), text="List Supplier Info", bg="#274D8B", fg='white', font=('Arial', 8, 'bold')).pack(side='left', anchor='nw', padx=5, pady=5)
+
+
+
+
+
+
+
+
+
+
 
 
 # ---- Update Customer Info (Tab 3) ----
@@ -306,6 +390,20 @@ tab3Btn = tk.Button(tab3left, command=lambda: updateInfo(tableFrameTab3), text="
                     font=('Arial', 14, 'bold')).pack(side='top', anchor='nw', padx=10, pady=10)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ---- Delete orders (Tab 4) ----
 tab4left = tk.Frame(tab4, width=350)
 tab4right = tk.Frame(tab4, width=350)
@@ -371,16 +469,31 @@ tab4Btn = tk.Button(tab4left, command=lambda: delOrders(tableFrameTab4), text="D
                     font=('Arial', 14, 'bold')).pack(side='top', anchor='nw', padx=10, pady=10)
 
 
+
+
+# ---- Predefined SQL Functions (i.e. Joins, Intersections, etc.)
+tab5left = tk.Frame(tab5, width=350)
+tab5right = tk.Frame(tab5, width=350)
+tab5left.pack(side='left', expand=True, fill='both')
+tab5right.pack(side='left', expand=True, fill='both')
+
+
+
+
+
+
 # pack each tab
 tab1.pack(fill='both', expand=True)
 tab2.pack(fill='both', expand=True)
 tab3.pack(fill='both', expand=True)
 tab4.pack(fill='both', expand=True)
+tab5.pack(fill='both', expand=True)
 
 # add tab frames to create notebook
 nb.add(tab1, text='Create clothing')
-nb.add(tab2, text='View suppliers')
+nb.add(tab2, text='Read Supplier''s Info/Products')
 nb.add(tab3, text='Update customer info')
 nb.add(tab4, text='Delete orders')
+nb.add(tab5, text= 'Predefined SQL Functions')
 
 window.mainloop()  # Needed At end of a python file for executable application to properly run
